9b418d1e0b1645cb0e5861ca8739651b5987c763
Vlad Stoica <vlad96stoica@gmail.com>
2018-04-07 13:27:10 +0300

    add support for arithm in group

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -406,6 +406,10 @@ class TestParser < Test::Unit::TestCase
     assert_understands "SELECT * FROM `a` ORDER BY COUNT(`a`.`id`) ASC"
   end
 
+  def test_order_count
+    assert_understands "SELECT * FROM `a` GROUP BY (`a` * `b`)"
+  end
+
   private
 
   def assert_sql(expected, given)
